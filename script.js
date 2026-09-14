04a52238402643e4a157bc532e63882a1ae72920
raphaelsonjohn-pixel <raphaelsonjohn005@gmail.com>
2026-09-14 14:58:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+function handleSubmit(form, successEl, resetFields){
+    form.addEventListener('submit', function(e){
+      e.preventDefault();
+      if(successEl){ successEl.classList.add('show'); }
+      form.reset();
+    });
+  }
+  handleSubmit(document.getElementById('heroForm'), null);
+  handleSubmit(document.getElementById('waitlistForm'), document.getElementById('formSuccess'));
